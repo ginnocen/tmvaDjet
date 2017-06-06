9ef0d9fdd606b364f838ffd841a1f1d71f306011
Jing <boundino@gmail.com>
2017-06-06 15:34:39 -0400

    outputsub can be different from inputsub

--- prepDsample/doprepDsample.sh
+++ prepDsample/doprepDsample.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-
 ##
-INPUTDIR="/export/d00/scratch/jwang/Djets/MC/"
-INPUTSUB="DjetFiles_20170506_pp_5TeV_TuneCUETP8M1_Dfinder_MC_20170404_pthatweight"
-OUTPUTDIR="/export/d00/scratch/jwang/Djets/MC/"
+INPUTDIR="/export/d00/scratch/jwang/Djets/data/"
+INPUTSUB="DjetFiles_pp_5TeV_HeavyFlavor_Dfinder_Data_20170405_dPt4tkPt1p5Alpha0p2Decay2_D0Dstar"
+OUTPUTDIR="/export/d00/scratch/jwang/Djets/data/"
+OUTPUTSUB="DjetFiles_pp_5TeV_HeavyFlavor_Dfinder_Data_20170405_dPt4tkPt1p5Alpha0p2Decay2_D0Dstar"
 
 JETPT=80
 JETETAMIN=0.3
@@ -21,7 +21,7 @@ tETAMAX2=`echo $JETETAMAX | awk -F "." '{print $2}'`
 INPUTFILENAME=${INPUTDIR}/${INPUTSUB}.root
 tJETETAMIN=${tETAMIN1:-0}p${tETAMIN2:-0}
 tJETETAMAX=${tETAMAX1:-0}p${tETAMAX2:-0}
-OUTPUTFILENAME=${OUTPUTDIR}/tmva_${INPUTSUB}_jetpt_${JETPT}_jeteta_${tJETETAMIN}_${tJETETAMAX}.root
+OUTPUTFILENAME=${OUTPUTDIR}/tmva_${OUTPUTSUB}_jetpt_${JETPT}_jeteta_${tJETETAMIN}_${tJETETAMAX}.root
 
 ##
 root -b -q 'prepDsample.C+('\"${INPUTFILENAME}\"','\"${OUTPUTFILENAME}\"','${JETPT}','${JETETAMIN}','${JETETAMAX}','${MAXEVTS}')'
