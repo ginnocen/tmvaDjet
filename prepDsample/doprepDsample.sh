09040097814a4f8617cd3514178f077f91e8ec59
Jing <boundino@gmail.com>
2017-06-13 14:59:04 -0400

    update doprepDsample.sh

--- prepDsample/doprepDsample.sh
+++ prepDsample/doprepDsample.sh
@@ -1,32 +1,45 @@
 #!/bin/sh
 
 ##
-INPUTDIR="/export/d00/scratch/jwang/Djets/MC/"
-INPUTSUB="DjetFiles_20170506_pp_5TeV_TuneCUETP8M1_Dfinder_MC_20170404_pthatweight"
-OUTPUTDIR="/export/d00/scratch/jwang/Djets/MC/"
-OUTPUTSUB="DjetFiles_20170506_pp_5TeV_TuneCUETP8M1_Dfinder_MC_20170404_pthatweight"
 
-#INPUTDIR="/export/d00/scratch/ginnocen/DjetFiles_HighPtJet80_pp_5TeV_Dfinder_2april_v1/"
-#INPUTSUB="merged"
-#OUTPUTDIR="/export/d00/scratch/jwang/Djets/data/"
-#OUTPUTSUB="DjetFiles_HighPtJet80_pp_5TeV_Dfinder_2april_v1"
+#
+INPUTDIR="/export/d00/scratch/ginnocen/DjetFiles_HighPtJet80_pp_5TeV_Dfinder_2april_v1/"
+INPUTSUB="merged"
+OUTPUTDIR="/export/d00/scratch/jwang/Djets/data/"
+OUTPUTSUB="DjetFiles_HighPtJet80_pp_5TeV_Dfinder_2april_v1"
 
+#
 JETPT=80
 JETETAMIN=0.3
 JETETAMAX=1.6
 MAXEVTS=-1
 
 ##
+
 #
-tETAMIN1=`echo $JETETAMIN | awk -F "." '{print $1}'`
-tETAMIN2=`echo $JETETAMIN | awk -F "." '{print $2}'`
-tETAMAX1=`echo $JETETAMAX | awk -F "." '{print $1}'`
-tETAMAX2=`echo $JETETAMAX | awk -F "." '{print $2}'`
+rt_float_to_string=-1
+float_to_string()
+{
+    if [[ $# -ne 1 ]]
+    then
+        echo -e "  \033[1;31mError:\033[0m invalid argument number - float_to_string()"
+        exit 1
+    fi
+    part1=`echo $1 | awk -F "." '{print $1}'`
+    part2=`echo $1 | awk -F "." '{print $2}'`
+    rt_float_to_string=${part1:-0}p${part2:-0}
+}
+
+##
+
 #
+float_to_string $JETETAMIN
+tJETETAMIN=$rt_float_to_string
+float_to_string $JETETAMAX
+tJETETAMAX=$rt_float_to_string
 INPUTFILENAME=${INPUTDIR}/${INPUTSUB}.root
-tJETETAMIN=${tETAMIN1:-0}p${tETAMIN2:-0}
-tJETETAMAX=${tETAMAX1:-0}p${tETAMAX2:-0}
 OUTPUTFILENAME=${OUTPUTDIR}/tmva_${OUTPUTSUB}_jetpt_${JETPT}_jeteta_${tJETETAMIN}_${tJETETAMAX}.root
 
 ##
+
 root -b -q 'prepDsample.C+('\"${INPUTFILENAME}\"','\"${OUTPUTFILENAME}\"','${JETPT}','${JETETAMIN}','${JETETAMAX}','${MAXEVTS}')'
