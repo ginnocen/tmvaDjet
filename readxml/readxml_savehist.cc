0ad74422ab03b72d15be1c0cd78013ab62a72556
Jing <boundino@gmail.com>
2017-07-21 16:42:44 -0400

    minor change

--- readxml/readxml_savehist.cc
+++ readxml/readxml_savehist.cc
@@ -5,17 +5,7 @@ void readxml_savehist(TString inputmcname, TString inputdataname, TString output
                       Float_t jetptmin, Float_t jetetamin, Float_t jetetamax,
                       Float_t ptmin, Float_t ptmax, Float_t drmin, Float_t drmax)
 {
-  gStyle->SetOptTitle(0);
-  gStyle->SetOptStat(0);
-  gStyle->SetEndErrorSize(0);
-  gStyle->SetMarkerStyle(20);
-  gStyle->SetTextSize(0.05);
-  gStyle->SetTextFont(42);
-  gStyle->SetPadRightMargin(0.043);
-  gStyle->SetPadLeftMargin(0.18);
-  gStyle->SetPadTopMargin(0.1);
-  gStyle->SetPadBottomMargin(0.145);
-  gStyle->SetTitleX(.0f);
+  setgstyle();
 
   // read weight file
   const char* filename = weightfile;
