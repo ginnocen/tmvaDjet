0246665a079ee755b3927a8316a855fdb8f56502
Jing <boundino@gmail.com>
2017-07-21 17:59:21 -0400

    update xjjuti.h

--- includes/xjjuti.h
+++ includes/xjjuti.h
@@ -12,6 +12,8 @@ namespace xjjuti
 {
   template<size_t N>
   int findibin(const float (*array_)[N], float element_);
+  template<size_t N>
+  int findiedge(const float (*array_)[N], float element_);
   std::string number_to_string(float param_);
   std::string number_to_string(double param_);
   std::string number_to_string(int param_);
@@ -19,14 +21,25 @@ namespace xjjuti
   std::string number_remove_zero(double param_);
 }
 
-
+//
 template<size_t N>
 int xjjuti::findibin(const float (*array_)[N], float element_)
 {
   int ielement = -1;
   for(std::size_t i=0;i<((sizeof(*array_)/sizeof((*array_)[0]))-1) && ielement<0;i++)
     {
-      if(element_>(*array_)[i] && element_<(*array_)[i+1]) ielement = i;
+      if(element_>=(*array_)[i] && element_<(*array_)[i+1]) ielement = i;
+    }
+  return ielement;
+}
+
+template<size_t N>
+int xjjuti::findiedge(const float (*array_)[N], float element_)
+{
+  int ielement = -1;
+  for(std::size_t i=0;i<(sizeof(*array_)/sizeof((*array_)[0])) && ielement<0;i++)
+    {
+      if(element_==(*array_)[i]) ielement = i;
     }
   return ielement;
 }
